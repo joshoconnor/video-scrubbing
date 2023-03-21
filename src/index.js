bd1973e962b21424851f68f532f06040e3465906
Josh O'Connor <72623228+joshoconnor@users.noreply.github.com>
2023-03-21 15:29:03 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { scroll } from "motion";
+
+const video = document.querySelector("video");
+video.pause();
+
+// Scrub through the video on scroll
+scroll(
+  ({ y }) => {
+    if (!video.readyState) return;
+    video.currentTime = video.duration * y.progress;
+  },
+  {
+    target: document.querySelector("article"),
+    offset: ["-200px", "end end"]
+  }
+);
